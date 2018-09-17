9b3a5c99517beab4f3080aea925f3ac69cbacd01
Leonid <leonid.karachevtsev@gmail.com>
2018-09-17 22:06:09 +0300

    create graphical interface

--- main.js
+++ main.js
@@ -1,5 +1,31 @@
 $(document).ready(function() {
     for (var i = 1; i <= 10; i++) {
-        $('#conditions').append("<option>" + i);
+        if (i == 3) {
+            $('#columns').append("<option selected>" + i);
+            $('#rows').append("<option selected>" + i);
+        }
+        $('#columns').append("<option>" + i);
+        $('#rows').append("<option>" + i);
     }
 });
+
+function setMatrixSize() {
+    $('#matrix').empty();
+    var needRows = $('#rows').val(),
+        needColumns = $('#columns').val();
+    for (var i = 0; i <= needRows; i++) {
+        if (i === 0) {
+            $('#matrix').append('<input type="text" size="3" class="matrix-input" disabled value="">');
+            $('#matrix').append('<input type="text" size="3" class="matrix-input" disabled value="const">');
+            for (var j = 1; j <= needColumns; j++) {
+                $('#matrix').append('<input type="text" size="3" class="matrix-input" disabled value="x' + j + '">');
+            }
+        } else {
+            $('#matrix').append('<input type="text" size="3" class="matrix-input" disabled value="x' + i + '">');
+            for (var j = 0; j <= needColumns; j++) {
+                $('#matrix').append('<input type="text" size="3" class="matrix-input">');
+            }
+        }
+        $('#matrix').append('<br>');
+    }
+}
